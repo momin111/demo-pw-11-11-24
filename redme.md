38d4ab596c0ca33f2d82e137c3c73cc57b85001d
saymabushra <saymabushra@gmail.com>
2025-02-12 00:26:51 +0200

    create test cases and added pipeline

---
+++ redme.md
@@ -0,0 +1,8 @@
+1. Check that place order button is enabled when correct data is filled and popup message is OK 
+2. When correct data is removed the button is disabled
+
+
+button = data-testid="submit-order"
+username = data-testid="username"
+email = data-testid="email"
+ok popup = #popup-message
--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,35 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test.beforeEach(async ({ page }) => {
+  const path = require('path');
+  const filePath = `file://${path.resolve('order.html')}`;
+  await page.goto(filePath);
+})
+test('Check that place order button is enabled when correct data is filled and popup message is OK', async ({ page }) => {
+  const orderButton = page.getByTestId("submit-order")
+  const usernameInput = page.getByTestId("username")
+  const emailInput = page.getByTestId("email")
+  const okPopUp = page.locator("//*[@id=\"popup-message\"]")
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
+  await expect(orderButton).toBeDisabled()
+  await usernameInput.fill("testuser")
+  await emailInput.fill("testemail@test.com")
+  await expect(orderButton).toBeEnabled()
+  await orderButton.click()
+  await expect(okPopUp).toBeVisible()
 });
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('When correct data is removed the button is disabled', async ({ page }) => {
+  const orderButton = page.getByTestId("submit-order")
+  const usernameInput = page.getByTestId("username")
+  const emailInput = page.getByTestId("email")
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await usernameInput.fill("testuser")
+  await emailInput.fill("testemail@test.com")
+  await expect(orderButton).toBeEnabled()
+  await usernameInput.fill("")
+  await emailInput.fill("")
+  await expect(orderButton).toBeDisabled()
 });
+
+
